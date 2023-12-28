7a584a0326fc413a5dce5dad1fd8c27b5bc4a2eb
Bikcode <bikcodeh@gmail.com>
2023-12-28 11:58:58 -0500

    fix justify content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ export const Home = () => {
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <Title text="Characters" />
       <SearchInput placeholder="characters" text={querySearch} onInputChange={(text) => setQuerySearch(text)} onSearch={handleOnSearch} />
-      <Grid mt={8} container justifyContent="space-between" gap={2} mb={2}>
+      <Grid mt={8} container sx={{ justifyContent: {  xs: 'center',sm: 'center', md: 'space-between' } }} gap={2} mb={2}>
         {
           rickAndMortyQuery.data?.results.map(character => (<CardCharacter key={character.id} character={character} />))
         }
